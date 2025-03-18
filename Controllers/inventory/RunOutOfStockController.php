6ca4b204c41b5670cc37e2625b9b78c763f4d6da
Sokha Sim <sokha.sim@student.passerellesnumeriques.org>
2025-03-18 20:50:37 +0700

    feat(out stock): display out stock product form[SRUM-262]

--- Controllers/inventory/RunOutOfStockController.php
+++ Controllers/inventory/RunOutOfStockController.php
@@ -1,10 +1,23 @@
 <?php
 
-class RunOutOfStockController extends BaseController {
-    public function runOutOfStock() {
+require_once ("Models/inventory/OutStockModel.php");
+class RunOutOfStockController extends BaseController
+{
+    public function runOutOfStock()
+    {
         $this->view('inventory/run_out_of_stock/run_out_of_stock');
     }
 
-   
-}
+    private $iteam;
+    public function __construct()
+    {
+        $this->iteam = new OutStockModel();
+    }
 
+    public function index()
+    {
+        $products = $this->iteam->getProducts();
+        $categories = $this->iteam->getCategories();
+        $this->view("inventory/run_out_of_stock/run_out_of_stock", ["products" => $products, "categories" => $categories]);
+    }
+}
---
+++ Models/inventory/OutStockModel.php
@@ -0,0 +1,41 @@
+<?php
+require_once "Database/Database.php";
+class OutStockModel
+{
+    private $pdo;
+
+    public function __construct()
+    {
+        $host = "localhost";
+        $dbname = "inventorydb";
+        $username = "root";
+        $password = "";
+
+        $this->pdo = new Database($host, $dbname, $username, $password);
+    }
+
+    public function getCategories()
+    {
+        try {
+            $stmt = $this->pdo->query("SELECT * FROM categories");
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        } catch (Exception $e) {
+            echo "Error fetching categories: " . $e->getMessage();
+            return [];
+        }
+    }
+
+    public function getProducts()
+    {
+        $products = $this->pdo->query("
+            SELECT products.*, 
+                   IFNULL(categories.name, '') AS category_name
+            FROM products
+            LEFT JOIN categories ON products.category_id = categories.category_id
+            WHERE products.stock_quantity = 0
+            ORDER BY products.product_id DESC
+        ");
+
+        return $products->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
