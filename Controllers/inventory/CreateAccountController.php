2e45af318d76e05c9c695eb629698e4b66779a9d
sreyneang <sreyneang.loeun@student.passerellesnumeriques.org>
2025-03-23 22:26:32 +0700

    feat(design_login_interface):update form login[SRUM:311]

---
+++ Controllers/inventory/CreateAccountController.php
@@ -0,0 +1,9 @@
+<?php
+
+class CreateAccountController extends BaseController {
+    public function create_account() {
+        $this->view('account/create_account');
+    }
+
+   
+}
--- views/login/logout.php
+++ views/login/logout.php
@@ -1,6 +1,8 @@
 <?php
-              session_start();
-              session_destroy(); // Ends the session
-              header("Location: login.php"); // Redirects to login page
-              exit();
-              ?>
+    session_start();
+    session_destroy(); // Ends the session
+    header("Location: login.php"); // Redirects to login page
+    exit();
+?>
+
+
