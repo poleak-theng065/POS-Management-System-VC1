8747577feb30ddc191c9918c6fab73f0b410f78c
Lynak <lynak.khat@student.passerellesnumeriques.org>
2025-04-02 09:57:44 +0700

    style(update):fix style of card in page import product

--- views/inventory/product_list/product_list.php
+++ views/inventory/product_list/product_list.php
@@ -368,37 +368,6 @@ if (isset($_SESSION['users']) && $_SESSION['users'] === true): ?>
                             <th>Category</th>
                             <th>Action</th>
                         </tr>
-<<<<<<< HEAD
-                    <?php endforeach ?>
-                     
-                </tbody>
-            </table>
-        </div>
-        <div class="d-flex justify-content-between align-items-center mt-3">
-            <div id="entriesInfo" class="text-muted">
-                Showing 1 to <?= count($products) ?> of <?= count($products) ?> entries
-            </div>
-            <nav>
-                <ul class="pagination" id="pagination">
-                    <li class="page-item disabled" id="prevPage">
-                        <a class="page-link" href="#" aria-label="Previous">
-                            <span aria-hidden="true">&laquo;</span>
-                        </a>
-                    </li>
-                    <li class="page-item active" id="page1">
-                        <a class="page-link" href="#">1</a>
-                    </li>
-                    <li class="page-item" id="nextPage">
-                        <a class="page-link" href="#" aria-label="Next">
-                            <span aria-hidden="true">&raquo;</span>
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-    </div>
-</div>
-=======
                     </thead>
                     <tbody id="switchTableBody">
                         <?php foreach ($products as $index => $product): ?>
@@ -411,7 +380,6 @@ if (isset($_SESSION['users']) && $_SESSION['users'] === true): ?>
                                 data-status="<?= $product['status'] ?>"
                                 data-stock="<?= $product['stock_quantity'] ?>"
                                 data-category="<?= !empty($product['category_name']) ? $product['category_name'] : 'No category' ?>">
->>>>>>> ed53797f2ea312bae1c26400055c556e944f655f
 
                                 <td><?= $product['barcode'] ?></td>
 
